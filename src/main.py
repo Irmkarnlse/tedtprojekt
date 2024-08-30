fb46a46868a9541ef8a3aae1ad42945a16ac3d82
Irmkarnlse <nils.kramer.1989@googlemail.com>
2024-08-30 21:34:24 +0200

    test

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print("Hallo, Welt!")
+import pyautogui
+import time
+
+# Warte ein paar Sekunden, um das Zielprogramm zu öffnen
+time.sleep(5)
+
+# Pfad zu deinem Bild
+button_image_path = '../buttons/test.PNG'
+
+# Suche das Bild auf dem Bildschirm
+try:
+    button_location = pyautogui.locateCenterOnScreen(button_image_path)
+    if button_location is not None:
+        # Bewege die Maus zur Position des Bildes und klicke
+        pyautogui.moveTo(button_location)
+        pyautogui.click()
+    else:
+        print("Knopf nicht gefunden.")
+except FileNotFoundError:
+    print(f"Bilddatei nicht gefunden: {button_image_path}")
+except Exception as e:
+    print(f"Ein Fehler ist aufgetreten: {e}")
